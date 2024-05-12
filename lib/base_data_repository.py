0c84ddb255f5cae399735f8587cd24503e3293f7
Andrew Shakespeare <shakeyshakespeare0@gmail.com>
2024-05-12 21:58:56 +0100

    Bug fixes and conditions

--- lib/base_data_repository.py
+++ lib/base_data_repository.py
@@ -19,7 +19,7 @@ class BaseDataRepository:
     def add_data(self, data_type, slug, **kwargs):
         all_data = self.get_data()
         for data in all_data[data_type]:
-            if get_slug(data['name']) == slug:
+            if get_slug(data['name']) == slug or data['code'] == kwargs['code']:
                 return False
         all_data[data_type].append(kwargs)
         with open('lib/base_data.json', 'w') as json_file:
@@ -30,6 +30,11 @@ class BaseDataRepository:
         all_data = self.get_data()
         for data in all_data[data_type]:
             if get_slug(data['name']) == slug:
+                for item in all_data[data_type]:
+                    if item['name'] != data['name'] and item['name'] == kwargs['name']:
+                        return False
+                    if item['code'] != data['code'] and item['code'] == kwargs['code']:
+                        return False
                 data.update(kwargs)
                 with open('lib/base_data.json', 'w') as json_file:
                     json.dump(all_data, json_file, indent=4)
